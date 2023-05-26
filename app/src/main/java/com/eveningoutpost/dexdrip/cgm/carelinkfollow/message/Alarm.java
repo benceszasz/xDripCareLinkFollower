8315155f2fbf1d105cabdda0c5e30b99b959efbd
benceszasz <bence.szasz@gmail.com>
2023-05-26 16:07:46 +0200

    Use LastAlarm as alternative source for timezone identification

--- app/src/main/java/com/eveningoutpost/dexdrip/cgm/carelinkfollow/message/Alarm.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/cgm/carelinkfollow/message/Alarm.java
@@ -9,7 +9,8 @@ public class Alarm {
     }
 
     public int code;
-    public Date datetime;
+    public String datetime;
+    public Date datetimeAsDate;
     public String type;
     public boolean flash;
     public String kind;
